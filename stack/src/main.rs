5e2090bd5b03cb0746ad5f6ca01b97c656620ef6
kira-e <kira-e@users.noreply.github.com>
2023-05-21 00:09:23 +0200

    Added a simple stack

---
+++ stack/src/main.rs
@@ -0,0 +1,64 @@
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+struct Stack<T> {
+    head: Option<Box<Node<T>>>,
+    size: i32,
+}
+
+impl<T> Stack<T> {
+    fn init() -> Stack<T> {
+        Stack { head: None, size: 0 }
+    }
+}
+
+pub trait Lifo<T> {
+    fn push(&mut self, value: T);
+    fn pop(&mut self) -> Option<T>;
+    fn size(&self) -> i32;
+}
+
+impl<T> Lifo<T> for Stack<T> {
+    fn push(&mut self, value: T) {
+        self.head = Some(Box::new(Node { value, next: self.head.take() }));
+        self.size += 1;
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        match self.head.take() {
+            None => None,
+            Some(root_box) => {
+                self.head = root_box.next;
+                self.size -= 1;
+                Some(root_box.value)
+            }
+        }
+    }
+
+    fn size(&self) -> i32 {
+        self.size
+    }
+}
+
+
+fn main() {
+    let mut stack = Stack::init();
+    assert!(stack.size() == 0);
+
+    stack.push(33);
+    assert!(stack.size() == 1);
+    assert!(stack.pop() == Some(33));
+
+    stack.push(1);
+    stack.push(2);
+
+    assert!(stack.size() == 2);
+    assert!(stack.pop() == Some(2));
+    assert!(stack.size() == 1);
+    assert!(stack.pop() == Some(1));
+    assert!(stack.size() == 0);
+
+    println!("Nice!");
+}
