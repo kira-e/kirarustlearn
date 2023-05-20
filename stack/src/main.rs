bbd3f5ae8a289b735ab972e6ab38350f73fa9860
kira-e <kira-e@users.noreply.github.com>
2023-05-21 00:21:49 +0200

    Wrap struct into a module

--- stack/src/main.rs
+++ stack/src/main.rs
@@ -1,50 +1,59 @@
-struct Node<T> {
-    value: T,
-    next: Option<Box<Node<T>>>,
-}
-
-struct Stack<T> {
-    head: Option<Box<Node<T>>>,
-    size: i32,
-}
-
-impl<T> Stack<T> {
-    fn init() -> Stack<T> {
-        Stack { head: None, size: 0 }
-    }
-}
-
-pub trait Lifo<T> {
-    fn push(&mut self, value: T);
-    fn pop(&mut self) -> Option<T>;
-    fn size(&self) -> i32;
-}
-
-impl<T> Lifo<T> for Stack<T> {
-    fn push(&mut self, value: T) {
-        self.head = Some(Box::new(Node { value, next: self.head.take() }));
-        self.size += 1;
+mod kirack {
+    struct Node<T> {
+        value: T,
+        next: Option<Box<Node<T>>>,
     }
 
-    fn pop(&mut self) -> Option<T> {
-        match self.head.take() {
-            None => None,
-            Some(root_box) => {
-                self.head = root_box.next;
-                self.size -= 1;
-                Some(root_box.value)
+    pub struct Stack<T> {
+        head: Option<Box<Node<T>>>,
+        size: i32,
+    }
+
+    impl<T> Stack<T> {
+        pub fn init() -> Stack<T> {
+            Stack {
+                head: None,
+                size: 0,
             }
         }
     }
 
-    fn size(&self) -> i32 {
-        self.size
+    pub trait Lifo<T> {
+        fn push(&mut self, value: T);
+        fn pop(&mut self) -> Option<T>;
+        fn size(&self) -> i32;
+    }
+
+    impl<T> Lifo<T> for Stack<T> {
+        fn push(&mut self, value: T) {
+            self.head = Some(Box::new(Node {
+                value,
+                next: self.head.take(),
+            }));
+            self.size += 1;
+        }
+
+        fn pop(&mut self) -> Option<T> {
+            match self.head.take() {
+                None => None,
+                Some(root_box) => {
+                    self.head = root_box.next;
+                    self.size -= 1;
+                    Some(root_box.value)
+                }
+            }
+        }
+
+        fn size(&self) -> i32 {
+            self.size
+        }
     }
 }
 
+use crate::kirack::Lifo;
 
 fn main() {
-    let mut stack = Stack::init();
+    let mut stack = kirack::Stack::init();
     assert!(stack.size() == 0);
 
     stack.push(33);
